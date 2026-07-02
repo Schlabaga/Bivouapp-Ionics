849bb759b0d0e1a4bd7a162b44ff4526b6958e80
Schlabaga <78259357+Schlabaga@users.noreply.github.com>
2026-07-02 18:55:15 +0200

    Supprime enfin le VRAI fichier environment.ts de GitHub

--- Bivouapp-ionic/src/app/environment/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  bypassAuth: true,
-  production:false,
-  supabaseUrl:'https://wluyebrakzkvftofkmks.supabase.co',
-  supabaseKey:'sb_publishable_16eRskirQkEtT34_RKDV6A_SxMTlpSa',
-  supabaseDirectConnectionString:'postgresql://postgres:FeuilleDeMouchoir26%@db.wluyebrakzkvftofkmks.supabase.co:5432/postgres',
-  googlePlaceApiKey:'AIzaSyDeuHUj0oPxkvo9fwBJh2lBrj8_COOH5DE',
-}
